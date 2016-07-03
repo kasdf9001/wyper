bd506222dd925e7f32e676a05e6aee8cc4066578
Kalle Kallio <katie.login@runbox.com>
2016-07-03 22:57:09 +0300

    Make back end front end

--- views/site/index.php
+++ views/site/index.php
@@ -2,6 +2,8 @@
 
 /* @var $this yii\web\View */
 
+use yii\helpers\Html;
+
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
@@ -12,4 +14,15 @@ $this->title = 'My Yii Application';
         <p class="lead">todo: make back-end</p>
     </div>
 
+    <div class="body-content">
+
+        <div class="row">
+
+            <p class="text-center">This does not work or do something sensible! Mostly for demo purposes.</p>
+
+            <p class="text-center"><?= Html::a('Loocations',['location/index']) ?></p>
+
+        </div>
+
+    </div>
 </div>
